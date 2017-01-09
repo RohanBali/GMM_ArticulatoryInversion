40e76c2c8d7ab8d89e676639e8ead1cafffb69a0
Rohan Bali <roh10roh@gmail.com>
2017-01-08 21:23:57 -0500

    RohanB - removed unnecessary files

--- isSilent.m
+++
@@ -1,8 +0,0 @@
-function bool = isSilent(acousticSegment,meanAcoustic)
-bool = 0;
-acousticSegment = acousticSegment - mean(acousticSegment);
-if sum(abs(acousticSegment)) < 0.25*meanAcoustic
-    bool = 1;
-end
-
-end
